779209e43c7338bad4dba295a565c6b4304b7e1f
Tonicha <tonicha_1998@hotmail.com>
2019-04-25 13:34:29 +0000

    Finished recipe module

--- foxtrotProject/src/main/java/org/academiadecodigo/tropadelete/foxtrot/model/Recipe.java
+++ foxtrotProject/src/main/java/org/academiadecodigo/tropadelete/foxtrot/model/Recipe.java
@@ -1,4 +1,76 @@
 package org.academiadecodigo.tropadelete.foxtrot.model;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "customer")
 public class Recipe {
+
+    private String name;
+    private String description;
+
+    @ManyToMany(
+
+            cascade = {CascadeType.ALL},
+
+            fetch = FetchType.EAGER
+    )
+    private List<Ingredient> ingredients = new ArrayList<>();
+
+    @ManyToOne
+    private Customer customer;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void addIngredient(Ingredient ingredient) {
+        ingredients.add(ingredient);
+        ingredient.setRecipe(this);
+    }
+
+    public void removeIngredient(Ingredient ingredient) {
+        ingredients.remove(ingredient);
+        ingredient.setRecipe(null);
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                "} " + super.toString();
+    }
 }
