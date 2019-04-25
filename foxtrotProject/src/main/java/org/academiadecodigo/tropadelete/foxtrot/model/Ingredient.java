a12ebd2ff48645423bfdddcda66b1b63ba998ad9
Luis Mesquita <luis.mesquit@gmail.com>
2019-04-25 15:29:34 +0000

    added ingredients model

--- foxtrotProject/src/main/java/org/academiadecodigo/tropadelete/foxtrot/model/Ingredient.java
+++ foxtrotProject/src/main/java/org/academiadecodigo/tropadelete/foxtrot/model/Ingredient.java
@@ -1,4 +1,38 @@
 package org.academiadecodigo.tropadelete.foxtrot.model;
 
-public class Ingredient {
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ingredient")
+public class Ingredient extends AbstractModel {
+
+    private String name;
+
+    @ManyToMany(
+
+            cascade = {CascadeType.ALL},
+            fetch = FetchType.EAGER,
+            mappedBy = "ingredients"
+    )
+    private List<Recipe> recipes = new ArrayList<>();
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        recipes.add(recipe);
+    }
+
 }
