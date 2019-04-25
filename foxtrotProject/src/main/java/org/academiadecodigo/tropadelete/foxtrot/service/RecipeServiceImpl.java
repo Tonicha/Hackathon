785069867bee973a75525472327174291e6d2d90
RMelo <rmcrazymod@gmail.com>
2019-04-25 18:10:14 +0000

    recipeService

--- foxtrotProject/src/main/java/org/academiadecodigo/tropadelete/foxtrot/service/RecipeServiceImpl.java
+++ foxtrotProject/src/main/java/org/academiadecodigo/tropadelete/foxtrot/service/RecipeServiceImpl.java
@@ -2,6 +2,7 @@ package org.academiadecodigo.tropadelete.foxtrot.service;
 
 import org.academiadecodigo.tropadelete.foxtrot.dao.IngredientDao;
 import org.academiadecodigo.tropadelete.foxtrot.dao.RecipeDao;
+import org.academiadecodigo.tropadelete.foxtrot.model.Ingredient;
 import org.academiadecodigo.tropadelete.foxtrot.model.Recipe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,9 +25,28 @@ public class RecipeServiceImpl implements RecipeService {
         this.ingredientDao = ingredientDao;
     }
 
-    public void addRecipe(){
-
-        Recipe recipe = new Recipe();
+    @Override
+    public void addRecipe(Recipe recipe){
         recipeDao.saveOrUpdate(recipe);
     }
+
+    @Override
+    public List<Recipe> search(String ingredient) {
+        List<Ingredient> ingredients = ingredientDao.findAll();
+
+        List<Recipe> recipeList = null;
+
+        for (Ingredient searchedIngredient : ingredients ) {
+            if (searchedIngredient.getName().equals(ingredient)){
+                recipeList = (searchedIngredient.getRecipes());
+            }
+        }
+
+        if (recipeList == null) {
+            return null;
+        }
+
+        return recipeList;
+    }
+
 }
