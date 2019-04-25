2f6b780c35e6977543d015d39d2d2c3e8a2eeb3e
RMelo <rmcrazymod@gmail.com>
2019-04-25 17:00:09 +0000

    ingredientService

--- foxtrotProject/src/main/java/org/academiadecodigo/tropadelete/foxtrot/service/IngredientService.java
+++ foxtrotProject/src/main/java/org/academiadecodigo/tropadelete/foxtrot/service/IngredientService.java
@@ -1,4 +1,8 @@
 package org.academiadecodigo.tropadelete.foxtrot.service;
 
+import org.academiadecodigo.tropadelete.foxtrot.model.Ingredient;
+
 public interface IngredientService {
+
+    Ingredient getIngridient(Integer id);
 }
--- foxtrotProject/src/main/java/org/academiadecodigo/tropadelete/foxtrot/service/IngredientServiceImpl.java
+++ foxtrotProject/src/main/java/org/academiadecodigo/tropadelete/foxtrot/service/IngredientServiceImpl.java
@@ -1,6 +1,7 @@
 package org.academiadecodigo.tropadelete.foxtrot.service;
 
 import org.academiadecodigo.tropadelete.foxtrot.dao.IngredientDao;
+import org.academiadecodigo.tropadelete.foxtrot.model.Ingredient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +11,12 @@ public class IngredientServiceImpl implements IngredientService {
     private IngredientDao ingredientDao;
 
     @Autowired
-    public IngredientDao getIngredientDao() {
-        return ingredientDao;
-    }
-
-    @Autowired
     public void setIngredientDao(IngredientDao ingredientDao) {
         this.ingredientDao = ingredientDao;
     }
+
+    public Ingredient getIngridient(Integer id) {
+        return ingredientDao.findById(id);
+    }
+
 }
