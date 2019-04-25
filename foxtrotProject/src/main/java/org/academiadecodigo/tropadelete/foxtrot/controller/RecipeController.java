e7232ee4679c1c547f5402199cce46f31479f3ee
Guilherme Silveira <guilhermesilveira@enta.pt>
2019-04-25 16:58:42 +0000

    added additional stuff to work on

--- foxtrotProject/src/main/java/org/academiadecodigo/tropadelete/foxtrot/controller/RecipeController.java
+++ foxtrotProject/src/main/java/org/academiadecodigo/tropadelete/foxtrot/controller/RecipeController.java
@@ -1,4 +1,12 @@
 package org.academiadecodigo.tropadelete.foxtrot.controller;
 
+import org.academiadecodigo.tropadelete.foxtrot.service.RecipeService;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class RecipeController {
+
+    private RecipeService recipeService;
+    private
+
 }
